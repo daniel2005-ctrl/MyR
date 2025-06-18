71ac997ccde895b49dc52c2978c552ef19f724f6
daniel2005-ctrl <jhonatancardozo80@gmail.com>
2025-06-18 07:48:47 -0500

    Delete test_limits.php

--- test_limits.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "post_max_size: " . ini_get('post_max_size') . "<br>";
-echo "upload_max_filesize: " . ini_get('upload_max_filesize') . "<br>";
-echo "max_file_uploads: " . ini_get('max_file_uploads') . "<br>";
-echo "memory_limit: " . ini_get('memory_limit') . "<br>";
-?>
